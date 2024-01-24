77056de4caab59635b1f73839efee4181fef19bb
Redtius <redtius@gmail.com>
2024-01-24 22:00:53 +0100

    created project

---
+++ client/src/assets/apiConfig.ts
@@ -0,0 +1,6 @@
+export const headers = {
+  'X-RapidAPI-Key': '1a00300a9amshe3436d8d93d8ce0p1399e3jsn41e770033dbb',
+  'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+}
+
+export const baseURL = 'https://api-football-v1.p.rapidapi.com/v3/'
