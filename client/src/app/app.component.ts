090994be4d0c386c1c9b8706dd4a9f9b60af667a
Redtius <redtius@gmail.com>
2024-01-25 16:05:12 +0100

    added matches page and header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatchesComponent } from './matches/matches.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatchesComponent],
+  imports: [CommonModule, RouterOutlet,MatchesComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MatchesComponent } from './matches/matches.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MatchesComponent,
+  },
+  {
+    path: 'stats',
+    component: MatchesComponent,
+  },
+];
