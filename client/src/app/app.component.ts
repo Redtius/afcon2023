f2409c0fed63749c41b24e7fa72a78d9e149e216
Redtius <redtius@gmail.com>
2024-01-27 20:39:56 +0100

    Added Footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,16 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { MatchesComponent } from './matches/matches.component';
 import { HeaderComponent } from './header/header.component';
 import { InfoheaderComponent } from './infoheader/infoheader.component';
-import { Route } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatchesComponent,HeaderComponent,InfoheaderComponent],
+  imports: [CommonModule,FooterComponent, RouterOutlet,MatchesComponent,HeaderComponent,InfoheaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
-  
+
 }
