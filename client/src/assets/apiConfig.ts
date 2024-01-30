6a90f9c0b8da89da64166035e37448639d708ee1
Reda Mountassir <120612095+Redtius@users.noreply.github.com>
2024-01-30 02:15:54 +0100

    Delete client/src/assets/apiConfig.ts

--- client/src/assets/apiConfig.ts
+++
@@ -1,6 +0,0 @@
-export const header = {
-  key: '1a00300a9amshe3436d8d93d8ce0p1399e3jsn41e770033dbb',
-  host: 'api-football-v1.p.rapidapi.com'
-}
-
-export const baseURL = 'https://api-football-v1.p.rapidapi.com/v3/'
